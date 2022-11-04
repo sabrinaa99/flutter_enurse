844b618e7977da4bfd29e922753bf6bbc3d70e3f
sabrinaa99 <88741614+sabrinaa99@users.noreply.github.com>
2022-11-04 12:32:04 +0800

    Create connection.php

---
+++ api_nurse_app/connection.php
@@ -0,0 +1,7 @@
+<?php
+    $serverHost = "localhost";
+    $user = "root";
+    $password = "";
+    $database = "userdata";
+
+    $connectNow = new mysqli($serverHost, $user, $password, $database);
